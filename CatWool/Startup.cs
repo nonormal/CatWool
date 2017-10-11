0cc3e5d26e61689443096fe9b2bef0d04cddc2e0
nonormal <ngocson.itlonely@gmail.com>
2017-10-11 22:12:28 +0700

    Add project files.

---
+++ CatWool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CatWool.Startup))]
+namespace CatWool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
